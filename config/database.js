590bb4784a70f1461fbb8afab8d4e5cf1d20bd05
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-05 16:52:10 +0100

    try 12

--- config/database.js
+++ config/database.js
@@ -1,24 +1,14 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
-        ssl: true,
+        useNullAsDefault: true,
       },
     },
   },
