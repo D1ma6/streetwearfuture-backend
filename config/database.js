796e11491ac490bcd51a74635cfa570c692133a0
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-05 14:53:15 +0100

    try 8

--- config/database.js
+++ config/database.js
@@ -1,41 +1,25 @@
 const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
         },
       },
-    };
-  }
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        ssl: true,
       },
     },
-  };
-};
+  },
+});
