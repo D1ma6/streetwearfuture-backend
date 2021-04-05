a89d3fe49bf2684c16d217985ffd360b83a9ba24
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-05 15:35:16 +0100

    try 10

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -14,5 +14,19 @@ module.exports = ({ env }) => {
       },
     };
   }
-  return {};
 };
+// module.exports = ({ env }) => {
+//   if (env("NODE_ENV") === "production") {
+//     return {
+//       upload: {
+//         provider: "cloudinary",
+//         providerOptions: {
+//           cloud_name: env("CLOUDINARY_NAME"),
+//           api_key: env("CLOUDINARY_KEY"),
+//           api_secret: env("CLOUDINARY_SECRET"),
+//         },
+//       },
+//     };
+//   }
+//   // ...
+// };
