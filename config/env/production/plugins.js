f17cc6341b4fad1fedceab0004a175ddb99638c4
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-05 15:43:46 +0100

    try 11

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,32 +1,32 @@
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    return {
-      upload: {
-        provider: "aws-s3",
-        providerOptions: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET"),
-          },
-        },
-      },
-    };
-  }
-};
 // module.exports = ({ env }) => {
 //   if (env("NODE_ENV") === "production") {
 //     return {
 //       upload: {
-//         provider: "cloudinary",
+//         provider: "aws-s3",
 //         providerOptions: {
-//           cloud_name: env("CLOUDINARY_NAME"),
-//           api_key: env("CLOUDINARY_KEY"),
-//           api_secret: env("CLOUDINARY_SECRET"),
+//           accessKeyId: env("AWS_ACCESS_KEY_ID"),
+//           secretAccessKey: env("AWS_ACCESS_SECRET"),
+//           region: env("AWS_REGION"),
+//           params: {
+//             Bucket: env("AWS_BUCKET"),
+//           },
 //         },
 //       },
 //     };
 //   }
-//   // ...
 // };
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      upload: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+      },
+    };
+  }
+  // ...
+};
